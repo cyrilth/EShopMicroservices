635cc9958dc846d84b3265d7daa5a1a75edfcd7c
Cyril Thomas <cyrilthomas3@gmail.com>
2024-06-09 00:51:34 -0400

    Added CQRS pattern, Docker support, and exception handling
    This commit introduces several significant changes. The Command Query Responsibility Segregation (CQRS) pattern has been implemented with the addition of interfaces for commands, queries, and their respective handlers. Docker support has been enhanced with updates to the Dockerfile, .dockerignore, and the addition of Docker Compose files. Exception handling has been improved with the addition of several new exception classes and a custom exception handler. Additionally, a new `LoggingBehavior` class has been added for logging requests and a `ValidationBehavior` class for request validation. NuGet package references related to FluentValidation, Mapster, and MediatR have been added to the `BuildingBlocks.csproj` and `Catalog.API.csproj` files. The application has been configured to use Carter, MediatR, FluentValidation, Marten, and health checks. New classes for handling product-related requests have been added. Lastly, configuration files and launch settings have been updated or added.

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,45 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var assembly = typeof(Program).Assembly;
+var postgresConnStr = builder.Configuration.GetConnectionString("Database");
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(postgresConnStr!);
+}).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(postgresConnStr!);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+app.Run();
