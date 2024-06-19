f4f9c441d2e6a631f508c3831a6e11e8b3724cb7
Cyril Thomas <cyrilthomas3@gmail.com>
2024-06-18 20:44:33 -0400

    Enhanced app with EF Core and updates
    Updated various NuGet packages across projects, including FluentValidation, MediatR, and more, to improve functionality and compatibility. Program.cs now includes database initialization logic for development environments, ensuring readiness before application startup. appsettings.json has been expanded with new database connection strings. DependencyInjection.cs saw the activation of previously commented MediatR service registration, enhancing application services.
    
    Introduced an ApplicationDbContext with configurations for new entities: Customer, Product, Order, and OrderItem, using Fluent API for detailed setup. Added DatabaseExtensions.cs for database initialization and seeding, alongside InitialData.cs for providing seed data. Implemented AuditableEntityInterceptor and DispatchDomainEventsInterceptor for auditing and domain event dispatching, respectively.
    
    Added a new EF Core migration `20240618232101_InitialCreate` for initial database schema setup. This migration includes the creation of tables for Customers, Products, Orders, and OrderItems with necessary keys and relationships. Updated ApplicationDbContextModelSnapshot to reflect these changes.
    
    Enhanced DependencyInjection.cs with registrations for EF Core services and interceptors. Introduced GlobalUsing.cs for streamlined namespace management. Updated Ordering.Infrastructure.csproj with necessary EF Core NuGet packages for SQL Server support. Modified docker-compose files to include a SQL Server container setup, facilitating database operations.
    
    This comprehensive update enhances the application's infrastructure by integrating Entity Framework Core, improving dependency management, and establishing a robust database initialization and seeding mechanism.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).HasConversion
+            (
+                customerId => customerId.Value, //When saving to the database, convert the CustomerId to a Guid
+                dbId => CustomerId.Of(dbId) //When reading from the database, convert the Guid to a CustomerId
+            );
+
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+            builder.Property(c => c.Email).HasMaxLength(255);
+
+            builder.HasIndex(c => c.Email).IsUnique();
+        }
+    }
+}
